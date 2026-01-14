566c382d053e5b8354dcb73e380d39f013a63cab
AdrianoCelentano <adrian.schaefer87@gmail.com>
2026-01-14 22:45:33 +0100

    List Loading state

--- app/src/main/java/com/adriano/sharetheimage/ui/home/HomeScreen.kt
+++ app/src/main/java/com/adriano/sharetheimage/ui/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.adriano.sharetheimage.ui.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,19 +18,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.CombinedLoadStates
+import androidx.paging.LoadState.Loading
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemContentType
 import androidx.paging.compose.itemKey
-import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
+import com.adriano.sharetheimage.R
 import com.adriano.sharetheimage.domain.model.Photo
 import com.adriano.sharetheimage.ui.shared.sharedBoundsWithLocalProviders
+import com.adriano.sharetheimage.ui.shared.shimmer
 
 @Composable
 fun HomeScreen(
@@ -52,7 +56,7 @@ fun HomeScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp),
-                label = { Text("Search") }
+                label = { Text(stringResource(R.string.search_label)) }
             )
 
             LazyColumn(
@@ -66,7 +70,14 @@ fun HomeScreen(
                 ) { index ->
                     val photo = photos[index]
                     if (photo != null) PhotoItem(photo, onPhotoClick)
+                    else PlaceHolder()
                 }
+
+                when {
+                    photos.loadState.isLoading -> listLoadingItems()
+                    photos.loadState.hasError -> errorItem()
+                }
+
             }
         }
 
@@ -80,19 +91,50 @@ fun PhotoItem(photo: Photo, onClick: (String) -> Unit) {
             .padding(4.dp)
             .clip(RoundedCornerShape(8.dp))
             .clickable { onClick(photo.id) }
-            .background(Color.LightGray)
             .sharedBoundsWithLocalProviders(key = photo.id)
     ) {
-        AsyncImage(
+        SubcomposeAsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(photo.urlSmall)
                 .crossfade(true)
                 .build(),
             contentDescription = photo.description,
             contentScale = ContentScale.Crop,
+            loading = { PlaceHolder() },
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
+                .height(PhotoListItemHeight.dp)
         )
     }
 }
+
+private fun LazyListScope.errorItem() {
+    item {
+        Text("Error")
+    }
+}
+
+
+@Composable
+fun PlaceHolder() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(PhotoListItemHeight.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .shimmer()
+    )
+}
+
+private fun LazyListScope.listLoadingItems() {
+    items(4) {
+        Box(modifier = Modifier.padding(4.dp)) {
+            PlaceHolder()
+        }
+    }
+}
+
+val CombinedLoadStates.isLoading: Boolean
+    get() = refresh is Loading || append is Loading
+
+const val PhotoListItemHeight = 200
