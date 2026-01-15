84c0c68fdb7b2bb0fa3f6e46e3ef55a9df64ac53
AdrianoCelentano <adrian.schaefer87@gmail.com>
2026-01-15 16:07:30 +0100

    debounce search query

--- app/src/main/java/com/adriano/sharetheimage/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/adriano/sharetheimage/ui/home/HomeViewModel.kt
@@ -9,11 +9,16 @@ import com.adriano.sharetheimage.domain.model.Photo
 import com.adriano.sharetheimage.domain.repository.PhotoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
@@ -21,11 +26,12 @@ class HomeViewModel @Inject constructor(
 ) : ViewModel() {
 
     val query: StateFlow<String> = savedStateHandle.getStateFlow(QueryKey, "Nature")
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    val photos: Flow<PagingData<Photo>> = query.flatMapLatest { query ->
-        repository.getSearchStream(query)
-    }.cachedIn(viewModelScope)
+    val photos: Flow<PagingData<Photo>> = query
+        .debounce(600L)
+        .distinctUntilChanged()
+        .filter { it.isNotBlank() }
+        .flatMapLatest { query -> repository.getSearchStream(query) }
+        .cachedIn(viewModelScope)
 
     fun onQueryChange(newQuery: String) {
         savedStateHandle[QueryKey] = newQuery
